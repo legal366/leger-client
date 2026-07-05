251f8c9d279e8d1fa13d764680b8f38c09bf1d90
dealdrive <dealdrivetechnology@gmail.com>
2026-07-05 17:00:44 +0100

    Added api url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://leger-api.onrender.com" || "/api",
+  baseURL: "https://leger-api.onrender.com/api" || "/api",
 });
 
 api.interceptors.request.use((config) => {
