7fef83ef55ab1fd07311bacc327114afb19fac57
dealdrive <dealdrivetechnology@gmail.com>
2026-07-30 12:43:30 +0100

    new update

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "http://localhost:5007/api" || "/api",
-  baseURL: "https://ref-recovery.onrender.com/api" || "/api",
+  baseURL: "https://leger-api.onrender.com/api" || "/api",
 });
 
 api.interceptors.request.use((config) => {
